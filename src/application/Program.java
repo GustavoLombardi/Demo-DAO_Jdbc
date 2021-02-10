16d9bc98755c4eb46648fd57351241e936e5869d
Gustavo Lombardi <gugagustavo46@gmail.com>
2021-02-10 08:18:01 -0300

    Insert

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -32,9 +33,17 @@ public class Program {
 			for (Seller obj : list) {
 
 				System.out.println(obj);
+				
+			}
+			
+				System.out.println(" ==== Test 5: seller Insert ======");
+				Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+				sellerdao.insert(newSeller);
+
+					System.out.println("Inserted! New Id = " +newSeller.getId());
 
 		}
 
 		}
 
-	}
+	
